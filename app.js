ec4a42609788d9a95484c78239c6a3c0ece59d0e
John McCambridge <johnmccambridge@Johns-MacBook-Pro.local>
2020-01-17 12:16:37 -0600

    add saving logic and basic questions for inquirer

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const https = require('https');
 const cli = require('commander');
 const inquirer = require('inquirer');
 const chalk = require('chalk');
-// const queryURL = "https://api.github.com/repos/pi-hole/pi-hole/issues";
+const fs = require('fs');
 
 console.log('Hi, welcome to Git Issues');
 let questions = [{
@@ -17,13 +17,48 @@ let questions = [{
     message: `What is the name of the Github repository? (Ex: https://github.com/microsoft/${chalk.red("vscode")}`,
     default: 'vscode'
 },
+{
+    type: 'checkbox',
+    message: 'Would you like to save this repository?',
+    name: 'save',
+    choices: [
+        // new inquirer.Separator(' = The Meats = '),
+        {
+            name: 'Yes'
+        },
+        {
+            name: 'No'
+        },
+    ],
+}]
 
-]
 inquirer
     .prompt(questions)
     .then(answers => {
         let queryURL = `https://api.github.com/repos/${answers.org}/${answers.repo}/issues`
-        console.log(queryURL)
+        if (answers.save.length === 0) {
+            answers.save = ['No']
+        }
+
+        if (answers.save[0] === 'Yes') {
+            fs.readFile('repos.json', function (err, data) {
+                var json = JSON.parse(data)
+                if (json.includes(queryURL)) {
+                    console.log("Repository is a duplicate, not saving")
+                    return
+                }
+                json.push(queryURL)
+
+                fs.writeFile("repos.json", JSON.stringify(json), (err, result) => {
+                    if (err) {
+                        console.log("Save error", err)
+                    }
+                    console.log("Saved!")
+                })
+            })
+        }
+        // console.log(queryURL)
+        // console.log(answers.save)
     });
 
 
